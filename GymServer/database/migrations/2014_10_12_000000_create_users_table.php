4eca732ca42b5bfb276868b910314b2c68b8ea5f
Sergio-Lp-G <sergiolpgar@gmail.com>
2022-01-20 18:00:46 +0000

    user??

--- GymServer/database/migrations/2014_10_12_000000_create_users_table.php
+++ GymServer/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('dni')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->float('weight');
+            $table->float('height');
+            $table->date('birthday');
+            $table->string('gender');
             $table->rememberToken();
             $table->timestamps();
         });
--- GymServer/resources/views/activities/index.blade.php
+++ GymServer/resources/views/activities/index.blade.php
@@ -1,61 +1,68 @@
-@extends('layout.app')
-@section('content')
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
-<h1>Lista de estudios</h1>
- -->
+    <h1>Lista de estudios</h1>
+    @extends('layout.app')
+    @section('content')
 
 
-<div class="container">
+    <div class="container">
 
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
 
-            <h1>Lista de estudios
-                <a href="/activities/create" class="btn btn-primary float-right">
-                    Nuevo
-                </a>
-            </h1>
+                <h1>Lista de estudios
+                    <a href="/activities/create" class="btn btn-primary float-right">
+                        Nuevo
+                    </a>
+                </h1>
 
 
-            <table class="table table-striped">
-                <tr>
-                    <th>Nombre</th>
-                    <th>Días</th>
-                    <th>Nº Sesiones</th>
-                    <th>Horario/Sesión</th>
-                    <th>Duración</th>
-                    <th>MaxParticipantes</th>
-                </tr>
-                {{-- @forelse ($activities as $activity)--}}
-                {{-- <tr>--}}
-                {{-- <td>{{$activity->code}} </td>--}}
-                {{-- <td>{{$activity->name}} </td>--}}
-                {{-- <td>{{$activity->abreviation}} </td>--}}
-                {{-- <td> <a class="btn btn-primary btn-sm" href="/activities/{{$activity->id}}">Ver</a></td>--}}
-                {{-- <td> <a class="btn btn-primary btn-sm" href="/activities/{{$activity->id}}/edit">Editar</a></td>--}}
-                {{-- </tr>--}}
-                {{-- @empty--}}
-                {{-- <tr>--}}
-                {{-- <td colspan="3">No hay estudios registrados</td>--}}
-                {{-- </tr>--}}
-                {{-- @endforelse--}}
-            </table>
+                <table class="table table-striped">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Días</th>
+                        <th>Nº Sesiones</th>
+                        <th>Horario/Sesión</th>
+                        <th>Duración</th>
+                        <th>MaxParticipantes</th>
+                    </tr>
+                    {{-- @forelse ($activities as $activity)--}}
+                    {{-- <tr>--}}
+                    {{-- <td>{{$activity->code}} </td>--}}
+                    {{-- <td>{{$activity->name}} </td>--}}
+                    {{-- <td>{{$activity->abreviation}} </td>--}}
+                    {{-- <td> <a class="btn btn-primary btn-sm" href="/activities/{{$activity->id}}">Ver</a></td>--}}
+                    {{-- <td> <a class="btn btn-primary btn-sm" href="/activities/{{$activity->id}}/edit">Editar</a></td>--}}
+                    {{-- </tr>--}}
+                    {{-- @empty--}}
+                    {{-- <tr>--}}
+                    {{-- <td colspan="3">No hay estudios registrados</td>--}}
+                    {{-- </tr>--}}
+                    {{-- @endforelse--}}
+                </table>
 
 
 
 
 
+            </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
+
+
+
+</body>
+
+</html>
