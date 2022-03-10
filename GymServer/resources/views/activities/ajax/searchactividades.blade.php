38026f3e384bf350a2f5fde99f9d2871f221fcf0
Adrian Ramos <30373697+AdrianRamosAyensa@users.noreply.github.com>
2022-03-10 16:25:47 +0100

    editar actvidades en la vista de ajax segun login

--- GymServer/resources/views/activities/ajax/searchactividades.blade.php
+++ GymServer/resources/views/activities/ajax/searchactividades.blade.php
@@ -1,21 +1,30 @@
 {{-- <h1>filtro: {{ var_dump($sesions) }}</h1> --}}
 <table class="table table-striped">
-<tr>
-    <th>Nombre</th>
-    <th>Descripción</th>
-    <th>Duración</th>
-    <th>Max. Participantes</th>
-    <th></th>
-    <th></th>
-</tr>
-<tr>
-    <td>{{$actividad['name']}} </td>
-    <td>{{$actividad['description']}} </td>
-    <td>{{$actividad['duration']}} </td>
-    <td>{{$actividad['participants']}} </td>
-    <td> <a class="btn btn-primary btn-sm" href="/activities/{{$actividad['id']}}">Ver</a></td>
-    <td> <a class="btn btn-primary btn-sm" href="/activities/{{$actividad['id']}}/edit">Editar</a></td>
-</tr>
+    <tr>
+        <th>Nombre</th>
+        <th>Descripción</th>
+        <th>Duración</th>
+        <th>Max. Participantes</th>
+        <th></th>
+        @auth
+            @if (Auth::user()->role_name == 'admin' )
+                <th></th>
+
+            @endif
+
+        @endauth
+    </tr>
+    <tr>
+        <td>{{$actividad['name']}} </td>
+        <td>{{$actividad['description']}} </td>
+        <td>{{$actividad['duration']}} </td>
+        <td>{{$actividad['participants']}} </td>
+        <td> <a class="btn btn-primary btn-sm" href="/activities/{{$actividad['id']}}">Ver</a></td>
+        @auth
+            @if (Auth::user()->role_name == 'admin' )
+            <td> <a class="btn btn-primary btn-sm" href="/activities/{{$actividad['id']}}/edit">Editar</a></td>
+            @endif
+
+        @endauth
+    </tr>
 </table>
-
-
