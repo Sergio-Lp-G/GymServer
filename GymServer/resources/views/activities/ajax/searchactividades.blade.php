2bbbcdf405c74d5d9917ce4f38603097a6e7e73f
Sergio-Lp-G <sergiolpgar@gmail.com>
2022-03-10 15:16:38 +0000

    dr

--- GymServer/resources/views/activities/ajax/searchactividades.blade.php
+++ GymServer/resources/views/activities/ajax/searchactividades.blade.php
@@ -1,21 +1,17 @@
 {{-- <h1>filtro: {{ var_dump($sesions) }}</h1> --}}
 <table class="table table-striped">
-<tr>
-    <th>Nombre</th>
-    <th>Descripción</th>
-    <th>Duración</th>
-    <th>Max. Participantes</th>
-    <th></th>
-    <th></th>
-</tr>
-<tr>
-    <td>{{$actividad['name']}} </td>
-    <td>{{$actividad['description']}} </td>
-    <td>{{$actividad['duration']}} </td>
-    <td>{{$actividad['participants']}} </td>
-    <td> <a class="btn btn-primary btn-sm" href="/activities/{{$actividad['id']}}">Ver</a></td>
-    <td> <a class="btn btn-primary btn-sm" href="/activities/{{$actividad['id']}}/edit">Editar</a></td>
-</tr>
+    <tr>
+        <th>Nombre</th>
+        <th>Descripción</th>
+        <th>Duración</th>
+        <th>Max. Participantes</th>
+        <th></th>
+    </tr>
+    <tr>
+        <td>{{$actividad['name']}} </td>
+        <td>{{$actividad['description']}} </td>
+        <td>{{$actividad['duration']}} </td>
+        <td>{{$actividad['participants']}} </td>
+        <td> <a class="btn btn-primary btn-sm" href="/activities/{{$actividad['id']}}">Ver</a></td>
+    </tr>
 </table>
-
-
